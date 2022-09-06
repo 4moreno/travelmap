3b9abfcf34e25f216cc4e4206616d28cc05443d6
Jose Bello <bellojosema11@gmail.com>
2022-09-06 11:36:19 +0100

    seeding data setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create([{ username: "Dareos", email: "dareos@dareos.com", password: "123456" }])
+City.create([{ name: "London" }, { name: "Berlin" }])
+Post.create(
+  user_id: "1",
+  city_id: "1",
+  title: "B", category: "movie",
+  description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna"
+)
+
+Post.create(
+  user_id: "1",
+  city_id: "2",
+  title: "Star Wars",
+  category: "movie",
+  description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna"
+)
