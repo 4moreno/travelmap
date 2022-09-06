8c213a5e87de377602ef3a681f2f7a961b9f4575
antonio moreno <amorenomtz@gmail.com>
2022-09-06 14:15:46 +0200

    this is a test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def create
+    @posts = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post), status: :see_other, notice: "You successfully created the post: #{@post.title}"
+    else
+      render 'new'
+    end
   end
 
   def new
@@ -18,5 +27,15 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :category, :address)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
