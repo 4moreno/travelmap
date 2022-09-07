d373ba5195a17ec69ddb42938f2d37a981b5f901
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-09-07 16:27:57 +0200

    created wishlist-method

--- app/policies/bookmark_policy.rb
+++ app/policies/bookmark_policy.rb
@@ -1,9 +1,11 @@
 class BookmarkPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      # scope.all
+      scope.where(user: user)
+      # @bookmarks[0].wishlist.user
+    end
   end
 
   def create?
