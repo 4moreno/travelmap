0072f67e896c268682b8d843cf598f8572c0a28c
antonio moreno <amorenomtz@gmail.com>
2022-09-07 17:29:33 +0200

    geocoding&mapbox done also removed the search bars and now they are in the navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,13 @@ class PostsController < ApplicationController
     else
       @posts = Post.all
     end
+    @markers = @posts.geocoded.map do |post|
+      {
+        lat: post.latitude,
+        lng: post.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { post: post })
+      }
+    end
   end
 
   def show
