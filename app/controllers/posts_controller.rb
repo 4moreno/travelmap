bae3641766a40f579b769c7753c25e8d7e9bb669
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-09-08 12:55:38 +0200

    fixed city_id and created category drop-down

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,9 +23,9 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user = current_user
-    @post.city = City.find(params[:post][:city_id])
     authorize @post
-    if @post.save
+    if @post.valid?
+      @post.save
       redirect_to post_path(@post), status: :see_other, notice: "You successfully created the post: #{@post.title}"
     else
       render 'new', status: :unprocessable_entity
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
+  CATEGORIES = ["Art&Culture", "Restaurant", "Outdoors", "Best spots"]
   belongs_to :city
   belongs_to :user
   has_many :bookmarks
   # validates :city, presence: true
-  # validates :address, presence: true
+  validates :title, presence: true
+  validates :address, presence: true
 
 
   include PgSearch::Model
@@ -16,6 +18,19 @@ class Post < ApplicationRecord
     tsearch: { prefix: true }
   }
 
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  before_validation :geocode, if: :will_save_change_to_address?
+
+  geocoded_by :address do |obj,results|
+    if geo = results.first
+      obj.latitude = geo.latitude
+      obj.longitude = geo.longitude
+      city_obj = City.find_by(name: geo.city)
+      # obj.city = city_obj ? city_obj : City.new(name: geo.city)
+      if city_obj
+        obj.city = city_obj
+      else
+        obj.city = City.new(name: geo.city)
+      end
+    end
+  end
 end
