d0793a4062171d44e66591eef384cb630494bae1
Jose Bello <bellojosema11@gmail.com>
2022-09-09 14:13:47 +0100

    Final map + Algolia

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -1,5 +1,17 @@
 class WishlistsController < ApplicationController
   def index
-    @bookmarks = policy_scope(Bookmark)
+    @wishlist = current_user.wishlist
+    authorize @wishlist
+    @posts = policy_scope(Post)
+    @posts = @wishlist.posts
+    # authorize @posts
+
+  # The `geocoded` scope filters only flats with coordinates
+      @markers = @posts.geocoded.map do |post|
+      {
+        lat: post.latitude,
+        lng: post.longitude
+      }
+   end
   end
 end
