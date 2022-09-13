6566ae6afd33a8a167355a1fc663906275215608
yukiko laurentiu <yuki.laurentiu@gmail.com>
2022-09-13 11:38:54 +0200

    create default user and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ ActiveRecord::Base.connection.tables.each do |t|
 end
 
 puts "delete DB"
-Bookmark.destroy_all
-Wishlist.destroy_all
-Post.destroy_all
-City.destroy_all
-User.destroy_all
+# Bookmark.destroy_all
+# Wishlist.destroy_all
+# Post.destroy_all
+# City.destroy_all
+# User.destroy_all
 
 User.create(username: "Dareos", email: "dareos@lewagon.com", password: "111111")
 # User.create(username: "Yuki", email: "yuki@lewagon.com", password: "222222")
