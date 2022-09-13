c45067a2b90e677e4085c1aad016886311b844fe
Jose Bello <bellojosema11@gmail.com>
2022-09-13 13:57:33 +0100

    Solving merging problems

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,6 +2,6 @@ class Wishlist < ApplicationRecord
   belongs_to :user
   has_many :bookmarks, dependent: :destroy
   has_many :posts, through: :bookmarks
-   geocoded_by :address
+  geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
