10cdaad7c1b41bed33137accfe7e77e0ace74e4a
antonio moreno <amorenomtz@gmail.com>
2022-09-15 08:50:57 +0200

    did some small changes with the feedback

--- app/channels/chatroom_channel.rb
+++ app/channels/chatroom_channel.rb
@@ -1,7 +1,7 @@
 class ChatroomChannel < ApplicationCable::Channel
   def subscribed
     chatroom = Chatroom.find(params[:id])
-    stream_for_chatroom
+    stream_for chatroom
   end
 
   def unsubscribed
